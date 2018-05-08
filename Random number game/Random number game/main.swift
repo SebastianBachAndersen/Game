7b76dbd2eb996b2746786ec2e6992abd6d79342d
dog <ulri1122@gmail.com>
2018-05-08 13:41:05 +0200

    shortning

--- Random number game/Random number game/main.swift
+++ Random number game/Random number game/main.swift
@@ -1,36 +1,21 @@
-
-
 import Foundation
-
 print("Hello, World!")
-
-
-
 func firstact(){
     print("first u need to have da luck of da pricense")
     print("\nfirst u need to pick da number im thinking")
     print("+-----------------+")
     print("| between 1 and 5 |")
     print("+-----------------+")
-    
     var number = arc4random_uniform(5)+1
-    //var number:String = "1" //------------------------------------------- debugging
-    
-    // lucktest
     func tryAgian(){
-        var response = readLine()
+        let response = readLine()
         if number == UInt32(response!){
              print("you lucky you can pass")
-        
         }else{
-            
             print("u not lucky try agian")
-            
             tryAgian()
         }
     }
-    
     tryAgian()
 }
-
 firstact()
