501059429ffe4b76a9ff734e0b0868c18180c297
dog <ulri1122@gmail.com>
2018-05-09 11:07:40 +0200

    Auto stash before merge of "master" and "origin/master"

--- Random number game/Random number game/main.swift
+++ Random number game/Random number game/main.swift
@@ -1,11 +1,7 @@
 import Foundation
 print("Hello, World!\nfirst u need to have da luck of da pricense\nfirst u need to pick da number im thinking\n+-----------------+\n| between 1 and 5 |\n+-----------------+")
     var number = arc4random_uniform(5)+1
-    func tryAgian(){
-        let response = readLine()
-        if number == UInt32(response!){
-             print("you lucky you can pass")
-        }else{
-            print("u not lucky try agian")
-            tryAgian()}}
+    func tryAgian(){let response = readLine()
+    if number == UInt32(response!){print("you lucky you can pass")} else {print("u not lucky try agian")
+        tryAgian()}}
     tryAgian()
